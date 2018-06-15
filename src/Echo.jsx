c226ab47d98e518a224041a31670a140d05e1417
Travis Meisenheimer <travis@indexoutofbounds.com>
2018-06-14 21:24:01 -0500

    Added echo service

---
+++ src/Echo.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class EchoComponent extends React.Component {
+  constructor() {
+    super();
+
+    this.state = { data: '' };
+  }
+
+  componentDidMount() {
+    axios.get('https://postman-echo.com/get?foo1=bar1&foo2=bar2')
+         .then((res) => {
+           console.log(res.data);
+           this.setState({ data: res.data });
+         });
+  }
+
+  render() {
+    return (
+      <pre>{ JSON.stringify(this.state.data) }</pre>
+    );
+  }
+}
